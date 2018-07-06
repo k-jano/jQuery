0cf7da28b557cea1a7d6009a4a8e774fbc795c0a
kjano10 <kuba.jano3@gmail.com>
2018-07-06 23:19:34 +0200

    different selectors

---
+++ #2 - selectors/main.js
@@ -0,0 +1,20 @@
+$(function(){
+    //$('h2,h3').css('border','solid 1px gray');
+
+    //$('#container').css('border','solid 1px gray');
+
+    //$('.lead').css('border','solid 1px gray');
+
+    //$('li:last').css('border','solid 1px gray');
+
+    //$('p:even').css('border','solid 1px gray');
+
+    //$('div em').css('border', 'solid 1px gray');
+
+    //$('div > p').css('border', 'solid 1px gray');
+
+    //$(':header').css('border', 'solid 1px gray');
+
+    $('div:contains("Item")').css('border', 'solid 1px gray');
+
+})
